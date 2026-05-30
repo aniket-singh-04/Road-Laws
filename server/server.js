3d18beddb8fb6356ef5bba457d544d949a4b1d89
ketan2035 <ketankumar147856@gmail.com>
2026-05-30 17:02:06 +0530

    databse  created

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+dotenv.config();
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//connect db
+connectDb();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
